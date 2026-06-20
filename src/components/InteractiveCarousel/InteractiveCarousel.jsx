4fadfaf2a51f03a9c3cb327365ea2d781021a193
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2026-06-20 19:13:05 -0300

    se agregan iconos al CarruselInteractive

--- src/components/InteractiveCarousel/InteractiveCarousel.jsx
+++ src/components/InteractiveCarousel/InteractiveCarousel.jsx
@@ -4,29 +4,46 @@ import { useGLTF, OrbitControls, Billboard } from '@react-three/drei'
 import gsap from 'gsap'
 import * as THREE from 'three'
 import './InteractiveCarousel.scss'
+import IconVacuno from '../Icons/IconVacuno'
+import IconChef from '../Icons/IconChef'
+import IconKmd from '../Icons/IconKmd'
+import IconAstillas from '../Icons/IconAstillas'
+import IconCarnes from '../Icons/IconCarnes'
 
 
 const slides = [
   {
     num: '01',
+    icon: (
+      <IconChef />
+    ),
     title: 'Sabores unicos',
     desc: 'En ALKAMADO somos especialistas con años de experiencia en el arte del ahumado. Dominamos la cocción en el kamado para lograr sabores y aromas únicos que no vas a conseguir con ningún otro método.',
     image: './Images/s1-slide-1.png',
   },
   {
     num: '02',
+    icon: (
+      <IconKmd />
+    ),
     title: 'Que es un Kamado',
     desc: 'Es un asador de origen Japonés, con la capacidad de absorción, retención y emisión de calor, controlada con presicion que lo vuelve muy versátil para asar, ahumar, hornear o grillar alimentos.',
     image: './Images/s1-slide-2.png',
   },
   {
     num: '03',
+    icon: (
+      <IconCarnes />
+    ),
     title: 'Opciones de Ahumados',
     desc: 'Nuestra propuesta abarca desde cortes vacunos, porcinos y de ave, hasta opciones más sutiles como pescados selectos, vegetales asados y pizzas artesanales.',
     image: './Images/s1-slide-3.png',
   },
   {
     num: '04',
+    icon: (
+      <IconAstillas />
+    ),
     title: 'Tipo de astillas',
     desc: 'Utilizamos diferentes tipos de astillas dependiendo del corte y el tipo de carne seleccionados, controlando la intensidad del ahumado para no invadir la delicadeza de su sabor. El tipo de astilla define el carácter de cada plato, aportándole aromas y sabores únicos.',
     image: './Images/s1-slide-4.png',
@@ -270,13 +287,13 @@ function InteractiveCarousel() {
         </Canvas>
 
         <div className="icarousel__nav">
-          {slides.map((_, i) => (
+          {slides.map((item, i) => (
             <button
               key={i}
               className={`icarousel__nav-btn ${current === i ? 'icarousel__nav-btn--active' : ''}`}
               onClick={() => handleSelect(i)}
             >
-              {i + 1}
+              {item.icon}
             </button>
           ))}
         </div>
@@ -293,7 +310,7 @@ function InteractiveCarousel() {
           )}
           
           <h3 className="icarousel__slide-title">
-            <span className="icarousel__slide-num">{slide.num}</span>
+            <span className="icarousel__slide-num">{slide.icon}</span>
             {slide.title.split('\n').map((line, i) => (
               <span key={i}>{line}<br /></span>
             ))}
